025eccdeba6ea9089acf06ec8835432ef9bde44b
Vitor Hugo Salgado <vitorhugo.salgado@thoughtworks.com>
2022-06-21 18:39:30 -0400

    test: tls conn

---
+++ test/tls_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/vitorsalgado/mocha"
+	"github.com/vitorsalgado/mocha/internal/assert"
+	"github.com/vitorsalgado/mocha/internal/testutil"
+	"github.com/vitorsalgado/mocha/matcher"
+)
+
+func TestTLS(t *testing.T) {
+	m := mocha.ForTest(t)
+	m.StartTLS()
+
+	// allow insecure https request
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	scoped := m.Mock(mocha.Get(matcher.URLPath("/test")).
+		Header("test", matcher.EqualTo("hello")).
+		Reply(mocha.OK()))
+
+	req := testutil.Get(m.Server.URL + "/test")
+	req.Header("test", "hello")
+
+	res, err := req.Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	assert.True(t, scoped.IsDone())
+}
