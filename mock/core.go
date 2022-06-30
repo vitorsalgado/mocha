0f59770d57f8dcc0b524bbb9db284abbd0535c06
Vitor Hugo Salgado <vitorhugo.salgado@thoughtworks.com>
2022-06-29 21:52:11 -0400

    refactor: mock pkg

---
+++ mock/core.go
@@ -0,0 +1,94 @@
+package mock
+
+import (
+	"io"
+	"net/http"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/vitorsalgado/mocha/matcher"
+	"github.com/vitorsalgado/mocha/params"
+)
+
+type (
+	Mock struct {
+		ID           int
+		Name         string
+		Priority     int
+		Expectations []any
+		Reply        Reply
+		Hits         int
+		Enabled      bool
+		PostActions  []PostAction
+
+		mu *sync.Mutex
+	}
+
+	PostActionArgs struct {
+		Request  *http.Request
+		Response *Response
+		Mock     *Mock
+		Params   *params.Params
+	}
+
+	PostAction interface {
+		Run(args PostActionArgs) error
+	}
+
+	Response struct {
+		Status  int
+		Header  http.Header
+		Cookies []*http.Cookie
+		Body    io.Reader
+		Delay   time.Duration
+		Err     error
+		Mappers []ResponseMapper
+	}
+
+	Reply interface {
+		Err() error
+		Build(*http.Request, *Mock, *params.Params) (*Response, error)
+	}
+
+	ResponseMapperArgs struct {
+		Request    *http.Request
+		Parameters *params.Params
+	}
+
+	ResponseMapper func(res *Response, args ResponseMapperArgs) error
+
+	Storage interface {
+		Save(mock *Mock)
+		FetchEligible() []*Mock
+		FetchAll() []*Mock
+		Delete(id int)
+		Flush()
+	}
+
+	ExpectationValuePicker[V any] func(r *matcher.RequestInfo) V
+
+	Expectation[V any] struct {
+		Name        string
+		Matcher     matcher.Matcher[V]
+		ValuePicker ExpectationValuePicker[V]
+		Weight      int
+	}
+
+	MatchResult struct {
+		NonMatched []string
+		Weight     int
+		IsMatch    bool
+	}
+)
+
+type autoID struct {
+	id int32
+}
+
+func (i *autoID) Next() int {
+	atomic.AddInt32(&i.id, 1)
+	return int(i.id)
+}
+
+var id = autoID{}
--- mock/mock.go
+++ mock/mock.go
@@ -2,89 +2,13 @@ package mock
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"reflect"
 	"sync"
-	"time"
 
 	"github.com/vitorsalgado/mocha/matcher"
-	"github.com/vitorsalgado/mocha/params"
 )
-
-type (
-	Mock struct {
-		ID           int
-		Name         string
-		Priority     int
-		Expectations []any
-		Reply        Reply
-		Hits         int
-		Enabled      bool
-		PostActions  []PostAction
-
-		mu *sync.Mutex
-	}
-
-	PostActionArgs struct {
-		Request  *http.Request
-		Response *Response
-		Mock     *Mock
-		Params   *params.Params
-	}
-
-	PostAction interface {
-		Run(args PostActionArgs) error
-	}
-
-	Response struct {
-		Status  int
-		Header  http.Header
-		Cookies []*http.Cookie
-		Body    io.Reader
-		Delay   time.Duration
-		Err     error
-		Mappers []ResponseMapper
-	}
-
-	Reply interface {
-		Err() error
-		Build(*http.Request, *Mock, *params.Params) (*Response, error)
-	}
-
-	ResponseMapperArgs struct {
-		Request    *http.Request
-		Parameters *params.Params
-	}
-
-	ResponseMapper func(res *Response, args ResponseMapperArgs) error
-
-	Storage interface {
-		Save(mock *Mock)
-		FetchEligible() []*Mock
-		FetchAll() []*Mock
-		Delete(id int)
-		Flush()
-	}
-
-	ExpectationValuePicker[V any] func(r *matcher.RequestInfo) V
-
-	Expectation[V any] struct {
-		Name        string
-		Matcher     matcher.Matcher[V]
-		ValuePicker ExpectationValuePicker[V]
-		Weight      int
-	}
-
-	MatchResult struct {
-		NonMatched []string
-		Weight     int
-		IsMatch    bool
-	}
-)
-
-var id = autoID{}
 
 func New() *Mock {
 	return &Mock{
