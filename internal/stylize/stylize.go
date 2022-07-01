ded3d6624b1dacf989c60f88f912ccadbfc6564d
Vitor Hugo Salgado <vitorhugo.salgado@thoughtworks.com>
2022-07-01 01:36:46 -0400

    refactor: adjust stylize pkg

--- internal/stylize/stylize.go
+++ internal/stylize/stylize.go
@@ -5,31 +5,44 @@ import (
 	"fmt"
 	"os"
 	"runtime"
-	"strconv"
-	"strings"
+)
+
+type style int
+
+const (
+	ColorBlack style = iota + 30
+	ColorRed
+	ColorGreen
+	ColorYellow
+	ColorBlue
+	ColorMagenta
+	ColorCyan
+	ColorWhite
 )
 
 const (
-	Reset = "\u001b[0m"
+	ColorBlackBright style = iota + 90
+	ColorRedBright
+	ColorGreenBright
+	ColorYellowBright
+	ColorBlueBright
+	ColorMagentaBright
+	ColorCyanBright
+	ColorWhiteBright
+)
 
-	Bright       = 1
-	ColorBlack   = 30
-	ColorRed     = 31
-	ColorGreen   = 32
-	ColorYellow  = 33
-	ColorBlue    = 34
-	ColorMagenta = 35
-	ColorCyan    = 36
-	ColorWhite   = 97
-	ColorGray    = 90
+const (
+	Reset style = iota
+	StyleBold
+	StyleDim
+	StyleItalic
+	StyleUnderline
+)
 
-	StyleBold          = 1
-	StyleDim           = 2
-	StyleItalic        = 3
-	StyleUnderline     = 4
-	StyleInverse       = 7
-	StyleHidden        = 8
-	StyleStrikethrough = 9
+const (
+	StyleInverse style = iota + 7
+	StyleHidden
+	StyleStrikethrough
 )
 
 var (
@@ -38,43 +51,37 @@ var (
 	_, isCI = os.LookupEnv("CI")
 )
 
-// Stylize stylizes the text using provided text style parameters
-func Stylize(s string, codes ...int) string {
+func Black(s string) string         { return stylize(s, ColorBlack, 39) }
+func BlackBright(s string) string   { return stylize(s, ColorBlackBright, 39) }
+func Red(s string) string           { return stylize(s, ColorRed, 39) }
+func RedBright(s string) string     { return stylize(s, ColorRedBright, 39) }
+func Green(s string) string         { return stylize(s, ColorGreen, 39) }
+func GreenBright(s string) string   { return stylize(s, ColorGreenBright, 39) }
+func Yellow(s string) string        { return stylize(s, ColorYellow, 39) }
+func YellowBright(s string) string  { return stylize(s, ColorYellowBright, 39) }
+func Blue(s string) string          { return stylize(s, ColorBlue, 39) }
+func BlueBright(s string) string    { return stylize(s, ColorBlueBright, 39) }
+func Magenta(s string) string       { return stylize(s, ColorMagenta, 39) }
+func MagentaBright(s string) string { return stylize(s, ColorMagentaBright, 39) }
+func Cyan(s string) string          { return stylize(s, ColorCyan, 39) }
+func CyanBright(s string) string    { return stylize(s, ColorCyanBright, 39) }
+func Gray(s string) string          { return stylize(s, ColorBlackBright, 39) }
+func White(s string) string         { return stylize(s, ColorWhite, 39) }
+func WhiteBright(s string) string   { return stylize(s, ColorWhiteBright, 39) }
+
+func Bold(s string) string          { return stylize(s, StyleBold, 22) }
+func Dim(s string) string           { return stylize(s, StyleDim, 22) }
+func Italic(s string) string        { return stylize(s, StyleItalic, 23) }
+func Underline(s string) string     { return stylize(s, StyleUnderline, 24) }
+func Inverse(s string) string       { return stylize(s, StyleInverse, 27) }
+func Hidden(s string) string        { return stylize(s, StyleHidden, 28) }
+func Strikethrough(s string) string { return stylize(s, StyleStrikethrough, 29) }
+
+// stylize stylizes the text using provided text style parameters
+func stylize(s string, open style, close int) string {
 	if noColor || isWin || isCI {
 		return s
 	}
 
-	c := make([]string, len(codes))
-	for i, code := range codes {
-		c[i] = strconv.Itoa(code)
-	}
-
-	return fmt.Sprintf("\u001b[%sm%s%s", strings.Join(c, ";"), s, Reset)
+	return fmt.Sprintf("\x1b[%dm%s\x1b[%dm", open, s, close)
 }
-
-func Black(s string) string         { return Stylize(s, ColorBlack) }
-func BlackBright(s string) string   { return Stylize(s, ColorBlack) }
-func Red(s string) string           { return Stylize(s, ColorRed) }
-func RedBright(s string) string     { return Stylize(s, ColorRed, Bright) }
-func Green(s string) string         { return Stylize(s, ColorGreen) }
-func GreenBright(s string) string   { return Stylize(s, ColorGreen, Bright) }
-func Yellow(s string) string        { return Stylize(s, ColorYellow) }
-func YellowBright(s string) string  { return Stylize(s, ColorYellow, Bright) }
-func Blue(s string) string          { return Stylize(s, ColorBlue) }
-func BlueBright(s string) string    { return Stylize(s, ColorBlue, Bright) }
-func Magenta(s string) string       { return Stylize(s, ColorMagenta) }
-func MagentaBright(s string) string { return Stylize(s, ColorMagenta, Bright) }
-func Cyan(s string) string          { return Stylize(s, ColorCyan) }
-func CyanBright(s string) string    { return Stylize(s, ColorCyan, Bright) }
-func Gray(s string) string          { return Stylize(s, ColorGray) }
-func GrayBright(s string) string    { return Stylize(s, ColorGray, Bright) }
-func White(s string) string         { return Stylize(s, ColorWhite) }
-func WhiteBright(s string) string   { return Stylize(s, ColorWhite, Bright) }
-
-func Bold(s string) string          { return Stylize(s, StyleBold) }
-func Dim(s string) string           { return Stylize(s, StyleDim) }
-func Italic(s string) string        { return Stylize(s, StyleItalic) }
-func Underline(s string) string     { return Stylize(s, StyleUnderline) }
-func Inverse(s string) string       { return Stylize(s, StyleInverse) }
-func Hidden(s string) string        { return Stylize(s, StyleHidden) }
-func Strikethrough(s string) string { return Stylize(s, StyleStrikethrough) }
