4c420119408c607fbd4c24d6ecfbebefde3cc96d
Vitor Hugo Salgado <vitorhugo.salgado@thoughtworks.com>
2022-07-01 00:17:01 -0400

    refactor: simplify recovery

--- internal/middleware/recover.go
+++ internal/middleware/recover.go
@@ -10,21 +10,13 @@ func Recovery(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if recovery := recover(); recovery != nil {
-				var msg string
-				switch x := recovery.(type) {
-				case string:
-					msg = x
-				case error:
-					msg = x.Error()
-				default:
-					msg = fmt.Sprintf("an unexpected error occured. %v", x)
-				}
+				msg := fmt.Sprintf("an unexpected error occured. %v", recovery)
 
 				log.Printf("panic: %v", recovery)
 
 				w.Header().Set("content-type", "text/plain")
 				w.WriteHeader(http.StatusTeapot)
-				_, _ = w.Write([]byte(fmt.Sprintf("%s - %s", http.StatusText(http.StatusTeapot), msg)))
+				w.Write([]byte(fmt.Sprintf("%s - %s", http.StatusText(http.StatusTeapot), msg)))
 			}
 		}()
 
