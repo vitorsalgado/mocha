8a06d21900376f2958701ffa717c4b9082010e0a
Vitor Hugo Salgado <2922948+vitorsalgado@users.noreply.github.com>
2022-12-29 00:46:50 +0100

    refactor: add ctx to params

--- params.go
+++ params.go
@@ -1,21 +1,23 @@
 package mocha
 
+import "context"
+
 // Params defines a contract for a generic parameters repository.
 type Params interface {
 	// Get returns the parameter by its key.
-	Get(k string) (any, bool)
+	Get(ctx context.Context, k string) (any, bool)
 
 	// GetAll returns all stored parameters.
-	GetAll() map[string]any
+	GetAll(ctx context.Context) map[string]any
 
 	// Set sets a parameter.
-	Set(k string, v any)
+	Set(ctx context.Context, k string, v any)
 
 	// Remove removes a parameter by its key.
-	Remove(k string)
+	Remove(ctx context.Context, k string)
 
 	// Has checks if a parameter with the given key exists.
-	Has(k string) bool
+	Has(ctx context.Context, k string) bool
 }
 
 type paramsStore struct {
@@ -27,24 +29,24 @@ func Parameters() Params {
 	return &paramsStore{data: make(map[string]any)}
 }
 
-func (p paramsStore) Get(key string) (any, bool) {
+func (p paramsStore) Get(_ context.Context, key string) (any, bool) {
 	val, ok := p.data[key]
 	return val, ok
 }
 
-func (p paramsStore) GetAll() map[string]any {
+func (p paramsStore) GetAll(_ context.Context) map[string]any {
 	return p.data
 }
 
-func (p paramsStore) Set(key string, dep any) {
+func (p paramsStore) Set(_ context.Context, key string, dep any) {
 	p.data[key] = dep
 }
 
-func (p paramsStore) Remove(key string) {
+func (p paramsStore) Remove(_ context.Context, key string) {
 	delete(p.data, key)
 }
 
-func (p paramsStore) Has(key string) bool {
+func (p paramsStore) Has(_ context.Context, key string) bool {
 	_, ok := p.data[key]
 
 	return ok
